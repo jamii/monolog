f9007b5c8e7acda23447358751726de4d3971069
Jamie Brandon <jamie@scattered-thoughts.net>
2016-03-10 20:30:18 +0000

    Hardcoded filters

--- src/cljs/monolog/core.cljs
+++ src/cljs/monolog/core.cljs
@@ -6,30 +6,44 @@
 
 (defonce log (atom []))
 
+(defonce current-filter (atom :all))
+
 (defn log! [entry] (swap! log conj entry))
 
-(def bots [
-  (fn [log]
-    (into {}
-      (for [[message ix] (map vector log (range))
-            :when (re-find #"#todo" (:contents message))
-            :when (not (some #(re-find (re-pattern (str "#done " ix)) (:contents %)) log))]
-            [ix [:button {:on-click (fn [event]
-                                      (.preventDefault event)
-                                      (log! {:username "todo" :contents (str "#done " ix)}))}
-                 "✓"]])))
-])
+(def filters
+  {:all (fn [log]
+          (for [[message ix] (map vector log (range))]
+            ix))
+   :todo (fn [log]
+           (for [[message ix] (map vector log (range))
+                 :when (re-find #"#todo" (:contents message))
+                 :when (not (some #(re-find (re-pattern (str "#done " ix)) (:contents %)) log))]
+             ix))})
+
+(defn reactions [log]
+  (into {}
+    (for [ix ((:todo filters) log)]
+          [ix [:button {:on-click (fn [event]
+                                    (.preventDefault event)
+                                    (log! {:username "todo" :contents (str "#done " ix)}))}
+                "✓"]])))
+
+(defn filter-chooser []
+  (into [:div] (for [filter (keys filters)]
+    ^{:key (str "filter-" filter)}
+    [:button {:style {:text-decoration (if (= filter @current-filter) "underline" "none")}
+              :on-click (fn [event]
+                          (reset! current-filter filter))}
+     (name filter) " "])))
 
 (defn messages []
-  (let [reactions (doall (for [bot bots] (bot @log)))]
-
-  [:div#messages
-   (for [[message ix] (map vector @log (range))]
-     (let [reaction (into [:span] (for [reaction reactions
-                                   :when (reaction ix)]
-                                   (reaction ix)))]
-    ^{:key (str "message-" ix)}
-    [:div [:span ix " "] [:span (:contents message)] reaction]))]))
+  (let [reactions (reactions @log)
+        ixes ((@current-filter filters) @log)]
+    [:div
+      (for [ix ixes]
+        (let [message (@log ix)]
+          ^{:key (str "message-" ix)}
+          [:div [:span ix " "] [:span (:contents message)] (reactions ix)]))]))
 
 (defn console []
   (let [contents (reagent/atom "")]
@@ -45,6 +59,7 @@
 
 (defn page []
   [:div
+   [filter-chooser]
    [messages]
    [console]])
 
